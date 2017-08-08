84611d1d183855b2ad2b70acfc236d40fccf5be9
Jazeps Basko <Jazeps.Basko@hogarthww.com>
2017-08-08 10:47:42 +0100

    Preparing release 0.3.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     setup(
         name="tinydav-fork",
         packages=["tinydav"],
-        version='0.3.1.dev0',
+        version='0.3.1',
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         author="Manuel Hermann",
